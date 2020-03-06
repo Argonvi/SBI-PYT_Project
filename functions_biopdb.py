69d1909553aa620ed3767c6f3ab396289ec7d21f
Argonvi <59646158+Argonvi@users.noreply.github.com>
2020-03-06 01:35:55 +0100

    Create functions_biopdb.py

---
+++ functions_biopdb.py
@@ -0,0 +1,17 @@
+#This file is for the record of useful functions for the project that can be found in the Bio.PDB package
+
+from Bio.PDB import *
+
+#Get the pdb structure into a python object
+parser = PDBParser()
+structure = parser.get_structure("protein_complex","6gmh.pdb")
+
+#Get the polypeptides
+
+ppb = PPBuilder()
+polypeptides = ppb.build_peptides(structure) #is a list of polypeptide objects
+
+#Get the sequences
+
+for pp in polypeptides:
+    pp.get_sequence() #is a sequence object, similar to a string
