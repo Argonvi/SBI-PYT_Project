153577274f6c6e06e25371e1ef214d134d26fd86
Paulagomis <60719236+Paulagomis@users.noreply.github.com>
2020-03-07 22:26:36 +0100

    Update logProgress.py

--- logProgress.py
+++ logProgress.py
@@ -9,7 +9,7 @@ def logStart(inputList):
     sys.stderr = open("./ComplexBuilder.log", "w")
     sys.stderr.write("\n                         ComplexBuilder\n")
     sys.stderr.write("  BUILDING OF COMPLEXES USING THE PAIRED INTERACTION OF ITS ELEMENTS\n")
-    sys.stderr.write("Written by Paula Gomis Rosa, Arturo González and Marta López Balastegui\n\n")
+    sys.stderr.write("Written by Paula Gomis Rosa, Arturo González Vilanova and Marta López Balastegui\n\n")
     print("Job starting time: ", dt_string, file=sys.stderr)
     sys.stderr.write("Input files:\n -PDB files: ")
     pdbs=list( inputList[i] for i in range(len(inputList)-1) )
