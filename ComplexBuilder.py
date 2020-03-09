14c0a1d5ffa0c15f593fe728995f49ed80060b27
Argonvi <59646158+Argonvi@users.noreply.github.com>
2020-03-09 18:08:38 +0100

    Add files via upload

--- ComplexBuilder.py
+++ ComplexBuilder.py
@@ -1,14 +1,18 @@
 import argparser
-import utilities
+from utilities import *
 import logProgress
-from our_module import *
+
 
 # Read the command line arguments and check format
 options = argparser.readArgs()
-inputFiles = utilities.checkInputs(options.infasta, options.inpdb)
+inputFiles = checkInputs(options.infasta, options.inpdb)
 if options.verbose: # if verbose is ON write progress in "ComplexBuilder.log"
     logProgress.logStart(inputFiles)
 
 pdb_files = inputFiles[:-1]
 fasta_file = inputFiles[-1]
-print(seq_finder(pdb_files,fasta_file))
+a = seq_finder(pdb_files,fasta_file)
+
+print(a)
+
+constructor(a)
