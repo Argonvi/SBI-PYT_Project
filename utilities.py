06c46a62e914d99eb652f4878bc7d6b54bad0419
Paulagomis <60719236+Paulagomis@users.noreply.github.com>
2020-03-21 17:23:59 +0100

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -145,7 +145,7 @@ def pdb_dna_to_sequence(chain):
         seq += res.get_resname()[-1]
     return seq
 
-def data_extraction(pdb_files, fasta_file, threshold = 0.99):
+def data_extraction(pdb_files, fasta_file, verb, threshold = 0.99):
     """Takes as input a list of pdb files and a fasta file
     Returns a dictionary of dictionaries. The primary key is the model, the secondary
     key is the chain_id in said model and the value is the fasta_id of said chain.
@@ -176,11 +176,17 @@ def data_extraction(pdb_files, fasta_file, threshold = 0.99):
                     big_dictionary[model][chain.get_id()] = fasta_id
                     break
 
-    valores = []
+    values = []
+    tag=0
     for lista in [list(a.values()) for a in big_dictionary.values()]:
-        valores += lista
+        values += lista
     for fasta_id in fasta_ids:
-        if fasta_id not in set(valores): print("Fasta sequence with ID %s was not found between PDB files."%fasta_id,file = sys.stderr)
+        if fasta_id not in set(values):
+            tag = 1
+            if verb: print("Fasta sequence with ID %s was not found between PDB files."%fasta_id,file = sys.stderr)
+    if tag:
+        raise SystemExit("Program stopped due to missing sequences.")
+        if fasta_id not in set(values): print("Fasta sequence with ID %s was not found between PDB files."%fasta_id,file = sys.stderr)
     return big_dictionary
 
 def seq_dictionary(data):
